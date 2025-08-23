82e82b74a51f3c9d33a4c425080f886801e732c9
Vadym <vadym3110mh@gmail.com>
2025-08-23 11:57:54 +0300

    Added updated all the files that didn't get into previous commit

--- meta-tables-db.sql
+++ meta-tables-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE tables (
+CREATE TABLE meta_tables (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  faculty VARCHAR(255) NOT NULL,
-  speciality_code VARCHAR(50) NOT NULL,
-  department VARCHAR(255) NOT NULL,
-  groups_count INT NOT NULL,
-  year INT NOT NULL,
-  created_at DATETIME NOT NULL,
-  updated_at DATETIME NOT NULL
+  tableName VARCHAR(255) NOT NULL,
+  faculty VARCHAR(255),
+  speciality_code VARCHAR(50),
+  department VARCHAR(255),
+  groups_count INT,
+  entry_year INT,
+  created_at varchar(32) NOT NULL,
+  updated_at varchar(32) NOT NULL
 );
 
